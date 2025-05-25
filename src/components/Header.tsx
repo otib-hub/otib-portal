e7d5e99c5de7daf806cfa4869043c3d220432d55
1manuelc <manuelwn21@gmail.com>
2025-05-25 17:17:40 -0300

    feat: wrap logo and title in Link component for improved navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ import otib_logo_dark from '../../public/otib/logo/logo-icon-neg.svg';
 import otib_logo_light from '../../public/otib/logo/logo-icon-pos.svg';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export default function Header() {
 	const { theme } = useTheme();
@@ -23,20 +24,22 @@ export default function Header() {
 	return (
 		<header className='w-full flex flex-col gap-5'>
 			<div className='w-full flex justify-between items-center'>
-				<div className='inline-flex gap-2 justify-start items-center'>
-					{mounted && (
-						<Image
-							src={otib_current_logo}
-							className='h-9 w-fit'
-							alt='OTIB logo'
-						/>
-					)}
-					<h1
-						className={`${ibmPlexSans.className} text-3xl font-semibold lg:text-4xl tracking-tight`}
-					>
-						OTIB
-					</h1>
-				</div>
+				<Link href='/'>
+					<div className='inline-flex gap-2 justify-start items-center hover:opacity-70 cursor-pointer transition-opacity'>
+						{mounted && (
+							<Image
+								src={otib_current_logo}
+								className='h-9 w-fit'
+								alt='OTIB logo'
+							/>
+						)}
+						<h1
+							className={`${ibmPlexSans.className} text-3xl font-semibold lg:text-4xl tracking-tight`}
+						>
+							OTIB
+						</h1>
+					</div>
+				</Link>
 
 				<ModeToggle />
 			</div>
