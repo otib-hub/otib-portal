f68b6820ae18855495bde03a4120b3d30297cd9b
1manuelc <manuelwn21@gmail.com>
2025-06-04 12:21:42 -0300

    feat: improve language cookie definition route

--- src/app/api/set-locale/route.ts
+++ src/app/api/set-locale/route.ts
@@ -1,20 +1,29 @@
-import { locales } from '@/i18n/config';
+import { isValidLocale } from '@/utils/is-valid-locale';
 import { NextRequest, NextResponse } from 'next/server';
 
-const COOKIE_NAME = 'user-locale';
-
 export async function POST(req: NextRequest) {
-	const { locale } = await req.json();
+	try {
+		const { locale } = await req.json();
 
-	if (!locales.includes(locale)) {
-		return NextResponse.json({ error: 'Invalid locale' }, { status: 400 });
+		if (!isValidLocale(locale)) {
+			return NextResponse.json({ error: 'Invalid locale' }, { status: 400 });
+		}
+
+		const response = NextResponse.json({ success: true });
+		response.cookies.set(process.env.LOCALE_COOKIE_NAME, locale, {
+			path: '/',
+			maxAge: 60 * 60 * 24 * 365, // 1 ano
+			httpOnly: false, // permite acesso via client
+			secure: process.env.NODE_ENV === 'production', // HTTPS apenas em produção
+			sameSite: 'lax', // Configuração de segurança
+		});
+
+		return response;
+	} catch (error) {
+		console.error('Erro ao definir locale:', error);
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 }
+		);
 	}
-
-	const response = NextResponse.json({ success: true });
-	response.cookies.set(COOKIE_NAME, locale, {
-		path: '/',
-		maxAge: 60 * 60 * 24 * 365,
-	});
-
-	return response;
 }
