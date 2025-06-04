0eb25f50ecc706379b1ff65f8637082a30870a2c
1manuelc <manuelwn21@gmail.com>
2025-06-04 12:19:05 -0300

    refactor: organize locale service functions and utilities and use new cookie name env variable

--- src/services/locale/get-user-locale.ts
+++ src/services/locale/get-user-locale.ts
@@ -2,13 +2,13 @@
 
 import { cookies, headers } from 'next/headers';
 import { Locale, defaultLocale } from '@/i18n/config';
+import { isValidLocale } from '@/utils/is-valid-locale';
 
 const HEADER_NAME = 'accept-language';
-const COOKIE_NAME = 'user-locale';
 
 export async function getUserLocale(): Promise<Locale> {
 	const cookieStore = await cookies();
-	const cookieLocale = cookieStore.get(COOKIE_NAME)?.value;
+	const cookieLocale = cookieStore.get(process.env.LOCALE_COOKIE_NAME)?.value;
 
 	if (cookieLocale && isValidLocale(cookieLocale)) {
 		return cookieLocale as Locale;
@@ -23,16 +23,4 @@ export async function getUserLocale(): Promise<Locale> {
 	if (headerLocale.startsWith('es')) return 'es';
 
 	return defaultLocale;
-}
-
-export async function setUserLocale(locale: Locale) {
-	const cookieStore = await cookies();
-	cookieStore.set(COOKIE_NAME, locale, {
-		path: '/',
-		maxAge: 60 * 60 * 24 * 365, // 1 year
-	});
-}
-
-function isValidLocale(locale: string): locale is Locale {
-	return ['en', 'pt-BR', 'es'].includes(locale);
 }
