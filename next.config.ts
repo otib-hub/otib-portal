456612a0f896e054978cab584cbab92971b1166c
1manuelc <manuelwn21@gmail.com>
2025-06-13 13:57:10 -0300

    chore: rename NEXT_APP_BASE_PATH to NEXT_PUBLIC_APP_BASE_PATH

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const nextConfig: NextConfig = {
 	devIndicators: false,
 	output: 'standalone',
-	basePath: process.env.NEXT_APP_BASE_PATH,
+	basePath: process.env.NEXT_PUBLIC_APP_BASE_PATH,
 	assetPrefix: process.env.NEXT_APP_ASSET_PREFIX,
+	// allowedDevOrigins: ['192.168.1.*'],
 };
 
 const withNextIntl = createNextIntlPlugin();
