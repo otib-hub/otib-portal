d099ceb2fd0237abebf1e73f087da7a5d5e84563
1manuelc <manuelwn21@gmail.com>
2025-06-16 16:24:49 -0300

    chore: remove "assetPrefix" from NextConfig

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
 	devIndicators: false,
 	output: 'standalone',
 	basePath: process.env.NEXT_PUBLIC_APP_BASE_PATH,
-	assetPrefix: process.env.NEXT_APP_ASSET_PREFIX,
 	// allowedDevOrigins: ['192.168.1.*'],
 };
 
