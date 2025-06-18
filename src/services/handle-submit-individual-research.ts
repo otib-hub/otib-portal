f06ea676163feb0929990e76a426669793670f89
1manuelc <manuelwn21@gmail.com>
2025-06-18 10:53:42 -0300

    refactor: improve redability in handleSubmitIndividualResearch

--- src/services/handle-submit-individual-research.ts
+++ src/services/handle-submit-individual-research.ts
@@ -4,7 +4,8 @@ import { formatErrors } from '@/utils/format-errors';
 type Payload = Partial<IndividualResearchFormType>;
 
 export async function handleSubmitIndividualResearch(values: Payload) {
-	const endpoint = process.env.NEXT_PUBLIC_APP_BASE_PATH + '/api/individual-research';
+	const endpoint =
+		process.env.NEXT_PUBLIC_APP_BASE_PATH + '/api/individual-research';
 
 	try {
 		const response = await fetch(endpoint, {
@@ -16,7 +17,6 @@ export async function handleSubmitIndividualResearch(values: Payload) {
 		});
 
 		const json = await response.json();
-
 		if (!response.ok) {
 			const errorMsg = `[${response.status}] ~ ${formatErrors(json)}`;
 			throw new Error(errorMsg);
