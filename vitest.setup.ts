d0015d323fde8ae2b44b74b55a6bae2639b53274
1manuelc <manuelwn21@gmail.com>
2025-07-28 14:29:31 -0300

    setup: add next/headers global mock for testing

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -5,6 +5,12 @@ import * as matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
 
+// mocks globais
+vi.mock('next/headers', () => ({
+	cookies: vi.fn(),
+	headers: vi.fn(),
+}));
+
 // roda apos cada teste limpando arvores do react e mocks
 afterEach(async () => {
 	cleanup();
