368e8646b1d750ce678f57eefb02294349535f3c
1manuelc <manuelwn21@gmail.com>
2025-08-12 01:33:56 -0300

    test: add mock implementation for console.warn in console.mock.ts

--- src/tests/console.mock.ts
+++ src/tests/console.mock.ts
@@ -1,3 +1,7 @@
 export const mockConsoleError = vi
 	.spyOn(console, 'error')
 	.mockImplementation(() => {});
+
+export const mockConsoleWarn = vi
+	.spyOn(console, 'warn')
+	.mockImplementation(() => {});
