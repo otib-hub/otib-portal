c7458165b13ee9378fb57d0e16c73780f5b4c9ec
1manuelc <manuelwn21@gmail.com>
2025-09-03 15:55:09 -0300

    feat: adds InteractiveMapCard in homepage using HeroMap client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,68 @@
-import { LocaleInitializer } from "@/components/layout/LocaleInitializer";
+import { LocaleInitializer } from '@/components/layout/LocaleInitializer';
 
-import { ChevronDown } from "lucide-react";
-import { Footer } from "@/components/layout/Footer";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
-import { ibmPlexSerif } from "@/styles/fonts";
-import { getTranslations } from "next-intl/server";
-import { Metadata } from "next";
-import { HomeBentoGrid } from "./_components/HomeBentoGrid";
+import { ChevronDown } from 'lucide-react';
+import { Footer } from '@/components/layout/Footer';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { Heading } from '@/components/ui/heading';
+import { ibmPlexSerif } from '@/styles/fonts';
+import { getTranslations } from 'next-intl/server';
+import { Metadata } from 'next';
+import { HomeBentoGrid } from './_components/HomeBentoGrid';
+import { HeroMap } from './_components/HeroMap';
 
 export async function generateMetadata(): Promise<Metadata> {
-	const t = await getTranslations("app.Home");
+	const t = await getTranslations('app.Home');
 	return {
-		title: t("page-title"),
+		title: t('page-title'),
 	};
 }
 
 export default async function Home() {
-	const t = await getTranslations("app.Home");
+	const t = await getTranslations('app.Home');
 
 	return (
 		<>
 			<main className="px-custom w-full flex flex-col items-start py-6 gap-8 md:py-8 md:gap-12 mb-10 md:mb-6">
 				<LocaleInitializer />
 
-				<section id="home-hero" className="w-full py-22 md:py-32">
+				<section
+					id="home-heading"
+					className="w-full flex flex-col md:flex-row gap-8"
+				>
 					<div
 						id="home-hero-heading"
-						className="flex flex-col items-center justify-center gap-6 text-center"
+						className="flex flex-col items-start justify-center gap-6 text-start"
 					>
 						<Heading.H1 className="font-medium! text-4xl md:text-5xl">
-							{`${t("heading.title").split("\n")[0]} `}
+							{`${t('heading.title').split('\n')[0]} `}
 							<span
 								className={`${ibmPlexSerif.className} font-semibold italic bg-gradient-to-r from-chart-2 to-chart-5 bg-clip-text text-transparent`}
 							>
-								{t("heading.title").split("\n")[1]}
+								{t('heading.title').split('\n')[1]}
 							</span>
 						</Heading.H1>
 
 						<p className="text-base md:text-lg lg:max-w-[72%] text-secondary-foreground">
-							{t("heading.description")}
+							{t('heading.description')}
 						</p>
 
 						<Link
-							title={t("buttons.about")}
+							title={t('buttons.about')}
 							className={`${buttonVariants({
-								variant: "link",
+								variant: 'link',
 							})} text-base md:text-lg`}
 							href="/about"
 						>
-							{t("buttons.about")}
+							{t('buttons.about')}
 						</Link>
+					</div>
+
+					<div
+						id="home-hero"
+						className="w-full order-first md:order-last"
+					>
+						<HeroMap />
 					</div>
 				</section>
 
