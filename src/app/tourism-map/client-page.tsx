c67474b7e018caf7464baa84102c7a8ab645eed9
1manuelc <manuelwn21@gmail.com>
2025-09-14 17:42:09 -0300

    refactor: removes InteractiveMapCard and use only IbiapabaMap in tourism-map page

--- src/app/tourism-map/client-page.tsx
+++ src/app/tourism-map/client-page.tsx
@@ -1,12 +1,26 @@
 'use client';
 
 import { Heading } from '@/components/ui/heading';
-import { Footer } from '@/components/layout/Footer';
-import { InteractiveMapCard } from '@/components/fragments/InteractiveMapCard';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { InformativePageWrapper } from '@/components/layout/InformativePageWrapper';
 import { useTranslations } from 'next-intl';
 import { LinkType } from '@/@types/link';
+import dynamic from 'next/dynamic';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Spinner } from '@/components/ui/spinner';
+import { convertToSlug } from '@/utils/convert-to-slug';
+
+const LazyIbiapabaMap = dynamic(
+	() => import('@/components/layout/IbiapabaMap'),
+	{
+		ssr: false,
+		loading: () => (
+			<Skeleton className="w-full md:w-[30%] min-h-[400px] md:min-h-[436px] lg:min-h-[512px] rounded-2xl flex items-center justify-center bg-transparent">
+				<Spinner />
+			</Skeleton>
+		),
+	},
+);
 
 export default function TourismMap() {
 	const [selectedCity, setSelectedCity] = useState<string | undefined>(
@@ -16,20 +30,36 @@ export default function TourismMap() {
 
 	const breadcrumbs: LinkType[] = [
 		{ title: t('meta.home.title'), url: '/otib' },
-		{ title: t('meta.tourism-map.title'), url: '/tourism-map' },
+		{ title: t('meta.tourism-map.title') },
 	];
+
+	const handleCityChange = useCallback(
+		(city: string | undefined) => {
+			if (city) {
+				const formatted = convertToSlug(city.toLowerCase());
+				setSelectedCity(formatted);
+			} else {
+				setSelectedCity(undefined);
+			}
+		},
+		[setSelectedCity],
+	);
 
 	return (
 		<InformativePageWrapper breadcrumbLinks={breadcrumbs}>
-			<main className="mt-6 bg-background relative px-custom rounded-2xl w-full flex flex-col items-start justify-start gap-10">
+			<div className="bg-background relative rounded-2xl w-full flex flex-col items-start justify-start gap-10">
 				<Heading.H1>{t('app.tourism-map.title')}</Heading.H1>
-				<InteractiveMapCard
-					selectedCity={selectedCity}
-					setSelectedCity={setSelectedCity}
-				/>
-			</main>
 
-			<Footer />
+				<div className="w-full h-full flex flex-col md:flex-row gap-8">
+					<LazyIbiapabaMap
+						selected={selectedCity}
+						onChangeSelected={handleCityChange}
+					/>
+					<div className="flex-1 rounded-2xl bg-card flex items-center justify-center text-muted-foreground">
+						{selectedCity ?? 'Selecione uma cidade'}
+					</div>
+				</div>
+			</div>
 		</InformativePageWrapper>
 	);
 }
