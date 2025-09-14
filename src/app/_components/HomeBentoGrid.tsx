35eb904199c71b325d06da545dc01252b0111668
1manuelc <manuelwn21@gmail.com>
2025-09-14 17:47:21 -0300

    refactor: update bulletins card for productions

--- src/app/_components/HomeBentoGrid.tsx
+++ src/app/_components/HomeBentoGrid.tsx
@@ -1,34 +1,39 @@
-import { TFunction } from "@/@types/next-intl";
-import { BentoCard } from "./BentoCard";
-import { ClipboardPenLineIcon, NewspaperIcon } from "lucide-react";
+import { TFunction } from '@/@types/next-intl';
+import { BentoCard } from './BentoCard';
+import { BoxIcon, ClipboardPenLineIcon } from 'lucide-react';
 
 type BentoGridProps = {
-	t: TFunction<"app.Home">;
+	t: TFunction<'app.Home'>;
 };
 
 export async function HomeBentoGrid({ t }: BentoGridProps) {
 	return (
 		<>
 			<BentoCard
-				title={t("sections.bento-cards.form-card.title")}
-				description={t("sections.bento-cards.form-card.description")}
+				title={t('sections.bento-cards.form-card.title')}
+				description={t('sections.bento-cards.form-card.description')}
 				cta={{
-					label: t("sections.bento-cards.form-card.button_action"),
-					href: "/researches/tourist-individual?referrer=portal-home",
+					label: t('sections.bento-cards.form-card.button_action'),
+					href: '/researches/tourist-individual?referrer=portal-home',
 				}}
 			>
 				<ClipboardPenLineIcon className="size-10" />
 			</BentoCard>
 
 			<BentoCard
-				title={t("sections.bento-cards.bulletins-card.title")}
-				description={t("sections.bento-cards.bulletins-card.description")}
+				title={t('sections.bento-cards.productions-card.title')}
+				description={t(
+					'sections.bento-cards.productions-card.description',
+				)}
 				cta={{
 					href: 'https://nupreds.ifce.edu.br/otib-boletins/boletim01-2025.pdf',
-					label: t("sections.bento-cards.bulletins-card.button_action")
-				}}>
-				<NewspaperIcon className="size-10" />
+					label: t(
+						'sections.bento-cards.productions-card.button_action',
+					),
+				}}
+			>
+				<BoxIcon className="size-10" />
 			</BentoCard>
 		</>
 	);
-};
+}
