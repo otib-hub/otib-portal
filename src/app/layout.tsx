1351c1fdae7d72333c7286cbd4afd3e95510c097
1manuelc <manuelwn21@gmail.com>
2025-09-14 17:41:10 -0300

    feat: adds general metadata to website layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,44 @@ import QueryProvider from '@/providers/QueryProvider';
 import NextTopLoader from 'nextjs-toploader';
 
 export const metadata: Metadata = {
-	title: 'OTIB - Protótipo',
-	description: 'Uma implementação inicial do OTIB',
+	title: 'Observatório do Turismo da Ibiapaba - OTIB',
+	description:
+		'Promovendo inovação e monitoramento sustentável do turismo na região da Ibiapaba."',
+	keywords: [
+		'OTIB',
+		'Observatório de Turismo da Ibiapaba',
+		'Observatório de Turismo',
+		'Ibiapaba',
+		'Serra da Ibiapaba',
+		'IFCE Tianguá',
+		'NUPREDS',
+		'turismo sustentável',
+		'dados turísticos',
+		'Ceará',
+		'desenvolvimento regional',
+		'inovação em turismo',
+	],
+	authors: [{ name: 'OTIB - NUPREDS' }],
+	creator: 'OTIB',
+	publisher: 'NUPREDS',
+	metadataBase: new URL('https://nupreds.ifce.edu.br/otib'),
+	openGraph: {
+		title: 'OTIB - Observatório de Turismo da Ibiapaba',
+		description:
+			'Projeto do IFCE Campus Tianguá voltado ao desenvolvimento sustentável e inovação no turismo da Ibiapaba.',
+		url: 'https://nupreds.ifce.edu.br/otib',
+		siteName: 'Observatório de Turismo da Ibiapaba',
+		// images: [
+		// 	{
+		// 		url: 'https://nupreds.ifce.edu.br/otib/images/otib-banner.png',
+		// 		width: 1200,
+		// 		height: 630,
+		// 		alt: 'OTIB - Observatório de Turismo da Ibiapaba',
+		// 	},
+		// ],
+		type: 'website',
+	},
+	alternates: { canonical: 'https://nupreds.ifce.edu.br/otib' },
 };
 
 export default async function RootLayout({
