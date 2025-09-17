b1ea2e426d78bf0361227165f62f49be762db1b8
1manuelc <manuelwn21@gmail.com>
2025-09-16 23:51:18 -0300

    style(button): remove unnecessary exclamation marks from inline-link class

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
 				ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
 				link: 'text-primary underline-offset-4 underline hover:no-underline',
 				'inline-link':
-					'text-primary underline-offset-4 underline hover:no-underline p-0! text-base! md:text-lg!',
+					'text-primary underline-offset-4 underline hover:no-underline p-0! text-base md:text-lg',
 			},
 			size: {
 				default: 'h-9 px-4 py-2 has-[>svg]:px-3',
