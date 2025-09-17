a9138c236d79d481389d589c9c14131b1a217b97
1manuelc <manuelwn21@gmail.com>
2025-09-17 01:32:48 -0300

    style(Button): enhance link variant styling for improved responsiveness

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
 				secondary:
 					'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-				link: 'text-primary underline-offset-4 underline hover:no-underline',
+				link: 'text-primary underline-offset-4 underline text-base md:text-lg hover:no-underline',
 				'inline-link':
 					'text-primary underline-offset-4 underline hover:no-underline p-0! text-base md:text-lg',
 			},
