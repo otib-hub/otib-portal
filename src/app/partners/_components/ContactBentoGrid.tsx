9a829cb25b4b5a2953e49b2e3965b45143531d65
1manuelc <manuelwn21@gmail.com>
2025-09-17 13:06:43 -0300

    feat(ContactBentoGrid): implements bento cards for contact channels

---
+++ src/app/partners/_components/ContactBentoGrid.tsx
@@ -0,0 +1,37 @@
+import { BentoCard } from '../../_components/BentoCard';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
+import { MailIcon } from 'lucide-react';
+import { TFunction } from '@/@types/next-intl';
+
+type ContactBentoGridProps = {
+	t: TFunction;
+};
+
+export function ContactBentoGrid({ t }: ContactBentoGridProps) {
+	return (
+		<>
+			<BentoCard
+				className="w-full lg:w-fit"
+				title={t(
+					'app.partners.section#join-us.ContactBentoCards.0.title',
+				)}
+				description={t(
+					'app.partners.section#join-us.ContactBentoCards.0.description',
+				)}
+				cta={
+					<Link
+						href={'mailto:nupreds@ifce.edu.br'}
+						className={`${buttonVariants({
+							variant: 'inline-link',
+						})} `}
+					>
+						{'nupreds@ifce.edu.br'}
+					</Link>
+				}
+			>
+				<MailIcon className="size-6" />
+			</BentoCard>
+		</>
+	);
+}
