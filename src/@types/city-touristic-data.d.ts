53cbb42c7318bb1da64c73d43c1dffae883cadb5
1manuelc <manuelwn21@gmail.com>
2025-09-21 17:55:10 -0300

    feat(CityTouristicData): refactor CityTouristicData type for improved structure and clarity

--- src/@types/city-touristic-data.d.ts
+++ src/@types/city-touristic-data.d.ts
@@ -1,19 +1,14 @@
 import { CityImage } from './city-image';
 
+type TourismStatistic = {
+	id: number;
+	name: string;
+	value: number;
+};
+
 export type CityTouristicData = {
-	id: string | number;
+	id: number;
 	name: string;
 	images: CityImage[];
-	companiesData: {
-		totalTourismCompanies: number;
-		registeredCadasturCompanies: number;
-		foodServices: number;
-		touristTransport: number;
-		lodging: number;
-		travelAgencies: number;
-		eventOrganizers: number;
-		tourGuidesAndOperators: number;
-		touristCamps: number;
-		themeParks: number;
-	};
+	companiesData: TourismStatistic[];
 };
