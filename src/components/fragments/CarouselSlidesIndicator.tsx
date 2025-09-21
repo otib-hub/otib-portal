097f8a34360206b08186085da06e6708f738534a
1manuelc <manuelwn21@gmail.com>
2025-09-21 17:54:03 -0300

    feat(CarouselSlidesIndicator): update styles and add className prop for customization

--- src/components/fragments/CarouselSlidesIndicator.tsx
+++ src/components/fragments/CarouselSlidesIndicator.tsx
@@ -4,15 +4,19 @@ type CarouselSlidesIndicatorProps = {
 	currentSlide: number;
 	totalSlides: number;
 	icon?: ReactNode;
+	className?: string;
 };
 
 export function CarouselSlidesIndicator({
 	currentSlide,
 	totalSlides,
 	icon,
+	className,
 }: CarouselSlidesIndicatorProps) {
 	return (
-		<span className="absolute bottom-4 left-4 p-2 flex items-center justify-center gap-2 bg-card/50 backdrop-blur-sm rounded-2xl shadow-md text-sm text-foreground/70 hover:opacity-50 transition-opacity">
+		<span
+			className={`absolute bottom-4 right-4 p-2 flex items-center justify-center gap-2 bg-background/70 backdrop-blur-sm rounded-2xl shadow-md text-sm text-foreground/70 hover:opacity-50 transition-opacity select-none ${className}`}
+		>
 			{icon}{' '}
 			<span>
 				{currentSlide} / {totalSlides}
